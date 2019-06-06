fc4399694318b3d6d2551c7134559afd38bc2b8b
Navdeep Singh <navdeep.er@gmail.com>
2019-06-06 14:55:22 +0530

    Update

--- falcon-agency/functions/index.js
+++ falcon-agency/functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // Copyright 2018, Google, Inc.
 // Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
@@ -18,6 +19,8 @@
 const {
   dialogflow,
   Suggestions,
+  Carousel,
+  Image,
 } = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
@@ -25,58 +28,72 @@ const functions = require('firebase-functions');
 
 
 // Instantiate the Dialogflow client.
-const app = dialogflow({debug: true});
+const app = dialogflow({ debug: true });
 
 // In the case the user is interacting with the Action on a screened device
 // The Fake Color Carousel will display a carousel of color cards
-// const managementCarousel = () => {
-//   const carousel = new Carousel({
-//     items: {
-//       'Daniel': {
-//         title: 'Daniel Endres',
-//         image: new Image({
-//           url: 'http://www.falcon-agency.com/user/themes/taita/img/team/daniel.jpg',
-//           alt: 'Daniel Endres',
-//         }),
-//       },
-//       'Max': {
-//         title: 'Max-F. Scheichenost',
-//         image: new Image({
-//           url: 'http://www.falcon-agency.com/user/themes/taita/img/team/max.jpg',
-//           alt: 'Max-F. Scheichenost',
-//         }),
-//       },
-//       'Kelvin': {
-//         title: 'Kelvin Koo',
-//         image: new Image({
-//           url: 'http://www.falcon-agency.com/user/themes/taita/img/team/kelvin.jpg',
-//           alt: 'Kelvin Koo',
-//         }),
-//       },
-//       'Soosan': {
-//         title: 'Soo San',
-//         image: new Image({
-//           url: 'http://www.falcon-agency.com/user/themes/taita/img/team/soo%20san.jpg',
-//           alt: 'Soo San',
-//         }),
-//       },
-//     },
-//   });
-//   return carousel;
-// };
+const managementCarousel = () => {
+  const carousel = new Carousel({
+    items: {
+      'Daniel': {
+        title: 'Daniel Endres',
+        image: new Image({
+          url: 'http://www.falcon-agency.com/user/themes/taita/img/team/daniel.jpg',
+          alt: 'Daniel Endres',
+        }),
+      },
+      'Max': {
+        title: 'Max-F. Scheichenost',
+        image: new Image({
+          url: 'http://www.falcon-agency.com/user/themes/taita/img/team/max.jpg',
+          alt: 'Max-F. Scheichenost',
+        }),
+      },
+      'Kelvin': {
+        title: 'Kelvin Koo',
+        image: new Image({
+          url: 'http://www.falcon-agency.com/user/themes/taita/img/team/kelvin.jpg',
+          alt: 'Kelvin Koo',
+        }),
+      },
+      'Soosan': {
+        title: 'Soo San',
+        image: new Image({
+          url: 'http://www.falcon-agency.com/user/themes/taita/img/team/soo%20san.jpg',
+          alt: 'Soo San',
+        }),
+      },
+    },
+  });
+  return carousel;
+};
 
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
   // eslint-disable-next-line max-len
   conv.ask(`Welcome! I can tell you about the agency, the number of employees or about the top management. Which would you like?`);
   conv.ask(new Suggestions(['About Agency', 'Top Management']));
-  // if (!conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT')) {
-  //   conv.ask('Sorry, try this on a screen device or select the ' +
-  //     'phone surface in the simulator.');
-  //   return;
-  // } else {
-  //   conv.ask(managementCarousel());
-  // }
+});
+
+app.intent('About Agency', (conv) => {
+  conv.ask('Falcon Agency is fastest growing digital agency in South-East Asia. What next?');
+  conv.ask(new Suggestions(['Top Management', 'Number of employees']));
+});
+
+app.intent('Number of employees', (conv) => {
+  conv.ask('We are team of 50. Anything else?');
+  conv.ask(new Suggestions(['Top Management', 'About Agency']));
+});
+
+app.intent('Top Management', (conv) => {
+  if (!conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT')) {
+    conv.ask('Sorry, try this on a screen device or select the ' +
+      'phone surface in the simulator.');
+    return;
+  } else {
+    conv.ask('These are our top management as follows:');
+    conv.ask(managementCarousel());
+  }
 });
 
 const SELECTED_ITEM_RESPONSES = {
